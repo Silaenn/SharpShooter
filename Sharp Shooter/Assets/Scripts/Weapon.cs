59d6d05f1c1d27a9736a9c2c9683a7558efddb8a
Deo silaen <deokeldisilaen@gmail.com>
2025-01-19 13:33:40 +0700

    ShootCooldown

--- Sharp Shooter/Assets/Scripts/Weapon.cs
+++ Sharp Shooter/Assets/Scripts/Weapon.cs
@@ -3,38 +3,20 @@ using StarterAssets;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
-    StarterAssetsInputs starterAssetsInputs;
-    const string SHOOT_STRING = "Shoot";
-    void Awake()
-    {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-    void Update()
-    {
-        HandleShoot();
 
-    }
 
-    private void HandleShoot()
+    public void Shoot(WeaponSO weaponSO)
     {
-        if (!starterAssetsInputs.shoot) return;
-
         muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
 
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
