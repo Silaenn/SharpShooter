08dea60a27a5cd87feccb2c5741a37a10c70d5af
Deo silaen <deokeldisilaen@gmail.com>
2025-01-21 03:37:32 +0700

    addplayerShieldUI

--- Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,21 +1,26 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] int startingHealth = 5;
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        AdjustShieldUI();
         Debug.Log(amount + " damage taken");
 
         if (currentHealth <= 0)
@@ -23,6 +28,21 @@ public class PlayerHealth : MonoBehaviour
             weaponCamera.parent = null;
             deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(gameObject);
+        }
+    }
+
+    void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 
